45a149dd178150e0b9ca602a4440a5e76a380805
ayakuma309 <ayako.30901@gmail.com>
2023-09-11 18:15:55 +0900

    ✨ feat: ログイン機能を作成

---
+++ front/app/app/components/auth/Login.tsx
@@ -0,0 +1,25 @@
+'use client'
+import React from 'react'
+import { useSession, signIn } from 'next-auth/react'
+
+const Login = () => {
+  const { data: session, status } = useSession()
+
+  if (status === 'loading') {
+    return <div>Loading...</div>
+  }
+
+  if (status !== 'authenticated') {
+    return (
+      <div>
+        <p>あなたはログインしていません</p>
+        <button onClick={() => signIn('google')}>
+          Googleでログイン
+        </button>
+      </div>
+    )
+  }
+  return null
+}
+
+export default Login
