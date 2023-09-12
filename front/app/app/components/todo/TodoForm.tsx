30419383fa643e33832a8c00b8442325f731d6e1
ayakuma309 <ayako.30901@gmail.com>
2023-09-12 11:46:18 +0900

    ✨ todo 新規作成

---
+++ front/app/app/components/todo/TodoForm.tsx
@@ -0,0 +1,65 @@
+'use client'
+
+import { useState } from 'react';
+import axios from 'axios';
+
+// Todoを作成するフォーム
+const TodoForm = () => {
+  // フォームの入力値を管理するstate
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+
+  // フォームの入力値を更新する関数
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      // APIを呼び出して、Todoを作成する
+      await axios.post('http://localhost:3000/api/v1/todos', {
+        todo: {
+          title,
+          description,
+        },
+      });
+      // Todoの作成に成功したら、フォームの入力値をリセットする
+
+      setTitle('');
+      setDescription('');
+      // ページをリロードする
+      window.location.reload();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className="space-y-6 w-3/4 max-w-lg py-10">
+      <form
+        onSubmit={handleSubmit}
+        className="space-y-6"
+      >
+        <label className="block text-xl font-bold text-gray-700">New Todo</label>
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder="タイトル"
+          className="block w-full py-2 pl-3 pr-4 text-base border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm"
+        />
+        <textarea
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          placeholder="本文"
+          className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm"
+        />
+        <button
+          type="submit"
+          className="mt-3 ml-auto flex justify-center py-2 px-8 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+        >
+          Create
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default TodoForm;
--- front/app/app/page.tsx
+++ front/app/app/page.tsx
@@ -1,10 +1,12 @@
 import Header from './components/common/Header';
+import TodoForm from './components/todo/TodoForm';
 import Todos from './components/todo/Todos';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       {/* <Header/> */}
+      <TodoForm />
       <Todos />
     </main>
   );
